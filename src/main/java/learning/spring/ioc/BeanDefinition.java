084efe2f15cecd846187d64c63224aafa8f9199c
jinghui.lin <jinghui.lin@zhenai.com>
2019-01-18 10:55:19 +0800

    手写spring ioc

---
+++ src/main/java/learning/spring/ioc/BeanDefinition.java
@@ -0,0 +1,22 @@
+package learning.spring.ioc;
+
+/**
+ * @Author Administrator
+ * @Date 2019/1/16
+ */
+public interface BeanDefinition {
+
+    final static String SINGLETON = "singleton";
+
+    final static String PROTOTYPE = "prototype";
+
+    Class<?> getBeanClass();
+
+    String getScope();
+
+    boolean isSingleton();
+
+    boolean isPrototype();
+
+    String getInitMethodName();
+}
---
+++ src/main/java/learning/spring/ioc/GenericBeanDefinition.java
@@ -0,0 +1,53 @@
+package learning.spring.ioc;
+
+import java.util.Objects;
+
+/**
+ * @Author Lin JingHui
+ * @Date 2019/1/16
+ */
+public class GenericBeanDefinition implements BeanDefinition {
+
+    private Class<?> beanClass;
+
+    private String scope = BeanDefinition.SINGLETON;
+
+    private String initMethodName;
+
+    public void setBeanClass(Class<?> beanClass) {
+        this.beanClass = beanClass;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public void setInitMethodName(String initMethodName) {
+        this.initMethodName = initMethodName;
+    }
+
+    @Override
+    public Class<?> getBeanClass() {
+        return beanClass;
+    }
+
+    @Override
+    public String getScope() {
+        return scope;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return Objects.equals(scope, BeanDefinition.SINGLETON);
+    }
+
+    @Override
+    public boolean isPrototype() {
+        return Objects.equals(scope, BeanDefinition.PROTOTYPE);
+    }
+
+    @Override
+    public String getInitMethodName() {
+        return initMethodName;
+    }
+}
