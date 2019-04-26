448a5b711b45bab507967ccf53755b0ce50737a8
jinghui.lin <jinghui.lin@zhenai.com>
2019-04-26 15:02:11 +0800

    add generics

---
+++ src/main/java/learning/generics/GenericExtend2.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C), 2005-2019, 深圳市珍爱网信息技术有限公司
+ */
+package learning.generics;
+
+/**
+ * @Description: TODO
+ * @Author: JingHui Lin
+ * @Date: 2019/4/26 14:09
+ * @Version V1.0
+ */
+public class GenericExtend2 {
+
+    public static <T extends Comparable> T getMax(T[] arr) {
+        T max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].compareTo(max) > 0) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static <T extends Comparable<T>> T getMax2(T[] arr) {
+        T max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].compareTo(max) > 0) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
---
+++ src/main/java/learning/generics/GenericExtends4.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C), 2005-2019, 深圳市珍爱网信息技术有限公司
+ */
+package learning.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description: TODO
+ * @Author: JingHui Lin
+ * @Date: 2019/4/26 14:45
+ * @Version V1.0
+ */
+public class GenericExtends4 {
+    private static class Fruit {
+    }
+
+    private static class Apple extends Fruit {
+    }
+
+    private static class Pear extends Fruit {
+    }
+
+    private static class FuShiApple extends Apple {
+    }
+
+    static class Clazz<T extends Fruit> {  //创建的类必须是Fruit的子类//为了自己类中使用这个类
+    }
+
+    public static void main(String[] args) {
+        Clazz<Fruit> t = new Clazz<>();
+        Clazz<Apple> t2 = new Clazz<>();
+        Clazz<Pear> t3 = new Clazz<>();
+        Clazz<FuShiApple> t4 = new Clazz<>();
+
+
+        //调用方法
+        List<? extends Fruit> list1 = new ArrayList<>();
+        add(list1);
+        List<Fruit> list2 = new ArrayList<>();
+        add(list2);
+        List<Apple> list3 = new ArrayList<>();
+        add(list3);
+        List<? extends Apple> list4 = new ArrayList<FuShiApple>();
+        add(list4);
+        List<FuShiApple> list5 = new ArrayList<>();
+        add(list5);
+
+
+        //？为什么错误 : 因为 ? 等同于？ extends Object :不是<= Fruit的 下面两个是一样的
+        List<?> list6 = new ArrayList<>();
+        List<? extends Object> list7 = new ArrayList<>();
+        //add(list6); // err
+        //add(list7); // err
+    }
+
+    /**
+     *  为了保证向下兼容的一致性，不能添加元素
+     */
+    public static void add(List<? extends Fruit> list) {
+        // 不能往里面加这样的对象 不能用于添加数据
+//        list.add(new Fruit());
+//        list.add(new Apple());
+//        list.add(new Pear());
+        list.add(null);
+    }
+}
